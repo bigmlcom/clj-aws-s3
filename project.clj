ad9a8704f4b900cfd614aea10a5f0b8b9de07998
Jose Antonio Ortega Ruiz <jao@gnu.org>
2012-03-21 21:27:59 +0100

    Versions bumped

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject clj-aws-s3 "0.2.1"
+(defproject com.bigml/clj-aws-s3 "0.3.0"
   :description "Clojure Amazon S3 library."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.0"]]
-  :dev-dependencies [[codox "0.4.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.amazonaws/aws-java-sdk "1.3.6"]])
