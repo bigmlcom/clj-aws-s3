26063313c72e399a7b1dd7cbd1b72f3d23dad2b2
Jose Antonio Ortega Ruiz <jao@gnu.org>
2012-05-01 00:33:12 +0200

    Versions bumped to latest java sdk and clojure 1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bigml/clj-aws-s3 "0.4.0"
+(defproject com.bigml/clj-aws-s3 "0.4.1"
   :description "Clojure Amazon S3 library."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.6"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.amazonaws/aws-java-sdk "1.3.8"]])
