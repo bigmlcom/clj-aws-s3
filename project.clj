876d2cdbfe3db99191f50de89c1a7bfb5c04b117
Jose Antonio Ortega Ruiz <jao@gnu.org>
2012-05-22 02:45:38 +0200

    Using aws jdk 1.3.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.bigml/clj-aws-s3 "0.4.1"
+(defproject com.bigml/clj-aws-s3 "0.4.2"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.8"]])
+                 [com.amazonaws/aws-java-sdk "1.3.10"]])
